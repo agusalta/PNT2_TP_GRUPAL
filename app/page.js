c6eac2482b2b40a5f6f27e7cd786afa8ff4183fb
agus <agusalta002@gmail.com>
2024-06-22 19:11:08 -0300

    upd header, movido a un nuevo archivo, nueva navbar con profile pic y botones de acceso a la sesion

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 import PageCocktails from "./cocktails/page";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="sticky top-0 z-50 bg-customWhite p-4 text-black text-center font-bold text-5xl italic">
-        COCKTAILS
-      </header>
+      <Header />
       <main className="flex-1 flex flex-col items-center justify-center p-4 bg-customWhite text-black">
-        <div className="w-full">
-          <PageCocktails />
-        </div>
+        <PageCocktails />
       </main>
     </div>
   );
