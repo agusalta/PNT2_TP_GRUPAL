b76349392b2ad7452a81e41616c6afed6c974804
agus <agusalta002@gmail.com>
2024-06-23 21:24:00 -0300

    add font League Spartan

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,16 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { CocktailProvider } from "./context/CocktailContext";
-import { UserProvider } from "./context/UserContext";
+import { League_Spartan } from '@next/font/google';
+import { UserProvider } from './context/UserContext';
+import { CocktailProvider } from './context/CocktailContext';
+import './globals.css'; // Asegúrate de importar tus estilos globales
 
-const inter = Inter({ subsets: ["latin"] });
+const leagueSpartan = League_Spartan({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export const metadata = {
-  title: "Proyecto PNT2",
-  description: "Integrantes del grupo: Agustín Altamirano y Mauro Francisco Pavesi",
+  title: 'Proyecto PNT2',
+  description: 'Integrantes del grupo: Agustín Altamirano y Mauro Francisco Pavesi',
 };
 
 export default function RootLayout({ children }) {
@@ -15,9 +18,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <UserProvider>
         <CocktailProvider>
-          <body className={inter.className}>{children}</body>
+          <body className={leagueSpartan.className}>
+            {children}
+          </body>
         </CocktailProvider>
       </UserProvider>
-    </html >
+    </html>
   );
 }
